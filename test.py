d3e5aedcb671b0878fe856711e986eca78aa9364
Marco Calamo (IlKaiser) <samuele.rinaldi90@gmail.com>
2020-10-30 20:38:52 +0100

    Add matplotlib

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ from Crypto.Util.Padding import pad,unpad
 import aes
 
 def test(plaintext,key):
+    my_res,res = [],[]
     print("##############################")
     print("TEST ECB")
     print("##############################")
@@ -17,6 +18,7 @@ def test(plaintext,key):
     my_e_time=end-start
     my_correct = dec == plaintext
     start = time.time()
+    my_res.append(my_e_time)
 
     cipher = AES.new(key, AES.MODE_ECB)
     ciphertext= cipher.encrypt(pad((plaintext),AES.block_size))
@@ -24,6 +26,7 @@ def test(plaintext,key):
     plain = unpad(cipher.decrypt(ciphertext),AES.block_size)
     end = time.time()
     e_time = end-start
+    res.append(e_time)
     correct= plaintext == plain
     print("My aes is "+str(my_correct))
     print("Pycriptodome aes is "+str(correct))
@@ -31,7 +34,6 @@ def test(plaintext,key):
     print("Pycriptodome elapsed time is "+str(e_time))
     print("Pycryptodome library is "+str(my_e_time/e_time) + " times faster than my aes" )
     print("##############################")
-
     print("TEST CBC")
     print("##############################")
     random = Random.new()
@@ -43,6 +45,7 @@ def test(plaintext,key):
     my_e_time=end-start
     my_correct = dec == plaintext
     start = time.time()
+    my_res.append(my_e_time)
 
     cipher = AES.new(key, AES.MODE_CBC)
     ct_bytes = cipher.encrypt(pad(plaintext, AES.block_size))
@@ -52,6 +55,7 @@ def test(plaintext,key):
     pt = unpad(cipher.decrypt(b64decode(ct)), AES.block_size)
     end = time.time()
     e_time = end-start
+    res.append(e_time)
     correct= plaintext == pt
     print("My aes is "+str(my_correct))
     print("Pycriptodome aes is "+str(correct))
@@ -59,7 +63,6 @@ def test(plaintext,key):
     print("Pycriptodome elapsed time is "+str(e_time))
     print("Pycryptodome library is "+str((my_e_time/e_time)) + " times faster than my aes" )
     print("##############################")
-
     print("TEST CFB")
     print("##############################")
     
@@ -70,6 +73,7 @@ def test(plaintext,key):
     my_e_time=end-start
     my_correct = dec == plaintext
     start = time.time()
+    my_res.append(my_e_time)
 
     cipher = AES.new(key, AES.MODE_CFB)
     ct_bytes = cipher.encrypt(pad((plaintext),AES.block_size))
@@ -78,6 +82,7 @@ def test(plaintext,key):
     cipher = AES.new(key, AES.MODE_CFB,b64decode(iv_))
     pt = unpad(cipher.decrypt(b64decode(ct)),AES.block_size)
     end = time.time()
+    res.append(e_time)
     e_time = end-start
     correct= plaintext == pt
     print("My aes is "+str(my_correct))
@@ -96,6 +101,7 @@ def test(plaintext,key):
     my_e_time=end-start
     my_correct = dec == plaintext
     start = time.time()
+    my_res.append(my_e_time)
 
     cipher = AES.new(key, AES.MODE_OFB)
     ct_bytes = cipher.encrypt(pad(plaintext,AES.block_size))
@@ -105,6 +111,7 @@ def test(plaintext,key):
     pt = unpad(cipher.decrypt(b64decode(ct)),AES.block_size)
     end = time.time()
     e_time = end-start
+    res.append(e_time)
     correct= plaintext == pt
     print("My aes is "+str(my_correct))
     print("Pycriptodome aes is "+str(correct))
@@ -123,6 +130,7 @@ def test(plaintext,key):
     my_e_time=end-start
     my_correct = dec == plaintext
     start = time.time()
+    my_res.append(my_e_time)
 
     cipher = AES.new(key, AES.MODE_CTR)
     ct_bytes = cipher.encrypt(pad(plaintext,AES.block_size))
@@ -132,6 +140,7 @@ def test(plaintext,key):
     pt = unpad(cipher.decrypt(b64decode(ct)),AES.block_size)
     end = time.time()
     e_time = end-start
+    res.append(e_time)
     correct= plaintext == pt
     print("My aes is "+str(my_correct))
     print("Pycriptodome aes is "+str(correct))
@@ -139,12 +148,42 @@ def test(plaintext,key):
     print("Pycriptodome elapsed time is "+str(e_time))
     print("Pycryptodome library is "+str((my_e_time/e_time)) + " times faster than my aes" )
     print("##############################")
+    return my_res,res
 
 print("128 bit test")
-test(b'a'*16,b'k'*16)
-print("1k bit test")
-test(b'a'*160,b'k'*16)
-print("100k bit test")
-test(b'a'*1600,b'k'*16)
-print("10M bit test")
-test(b'a'*160000,b'k'*16)
+my128,c128=test(b'a'*16,b'k'*16)
+print("1kB test")
+my1k,c1k=test(b'a'*1000,b'k'*16)
+print("100kB test")
+my100k,c100k=test(b'a'*100000,b'k'*16)
+print("10MB test")
+my10M,c10M=test(b'a'*10000000,b'k'*16)
+
+import numpy as np
+import matplotlib.pyplot as plt
+data = [my128,c128]
+
+X = np.arange(5)
+
+
+fig,ax = plt.subplots(1, 3)
+
+ax[0].bar(X, my1k, 0.35, label='myaes')
+ax[0].bar(X + 0.35, c1k, 0.35,label='pycrypto')
+ax[0].set_xticks(X + 0.35 / 2, ('ECB', 'CBC', 'CFB', 'OFB', 'CTR'))
+ax[0].legend(loc='best')
+ax[0].set_title("1k test (lower is better)")
+ax[1].bar(X, my100k, 0.35, label='myaes')
+ax[1].bar(X + 0.35, c100k, 0.35,label='pycrypto')
+ax[1].set_xticks(X + 0.35 / 2, ('ECB', 'CBC', 'CFB', 'OFB', 'CTR'))
+ax[1].legend(loc='best')
+ax[1].set_title("100k test (lower is better)")
+ax[2].bar(X, my10M, 0.35, label='myaes')
+ax[2].bar(X + 0.35, c10M, 0.35,label='pycrypto')
+ax[2].set_xticks(X + 0.35 / 2, ('ECB', 'CBC', 'CFB', 'OFB', 'CTR'))
+ax[2].legend(loc='best')
+ax[2].set_title("10M test (lower is better)")
+
+#plt.setp(ax, xticks=['ECB', 'CBC', 'CFB', 'OFB', 'CTR'])
+
+plt.show()
